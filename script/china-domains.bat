5ae896b77b1eaf5587540c14d6772c0ce1430369
peaceshi <peaceshi@outlook.com>
2020-03-10 22:29:33 +0800

    use jsdelivr

--- script/china-domains.bat
+++ script/china-domains.bat
@@ -1,4 +1,4 @@
-wsl wget -c https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf
+wsl wget -c https://cdn.jsdelivr.net/gh/felixonmars/dnsmasq-china-list@master/accelerated-domains.china.conf
 wsl sed -i 's/server=/domain-rules /g' ./*.conf
 wsl sed -i 's/114.114.114.114/ -nameserver china/g' ./*.conf
 wsl mv ./*.conf ../../../etc/smartdns/
