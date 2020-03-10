d54af371a31c61b54996e3f2d99076bb6f222481
peaceshi <peaceshi@outlook.com>
2020-03-10 22:29:09 +0800

    download smartdns rule directly

--- script/adblock.bat
+++ script/adblock.bat
@@ -1,4 +1,2 @@
-wsl wget -c https://cdn.jsdelivr.net/gh/privacy-protection-tools/anti-AD@master/adblock-for-dnsmasq.conf
-wsl sed -i 's/$/#/g' ./*.conf
-wsl sed -i 's/=/ /g' ./*.conf
-wsl mv ./*.conf ../../../etc/smartdns/
+wsl wget -c https://gitee.com/privacy-protection-tools/anti-ad/raw/master/anti-ad-for-smartdns.conf
+wsl mv ./anti-ad-for-smartdns.conf ../../../etc/smartdns/
