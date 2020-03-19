9b623431318bfe08c63146576e944be8f69b1235
peaceshi <peaceshi@outlook.com>
2020-03-19 13:15:26 +0800

    fix

--- script/restart.bat
+++ script/restart.bat
@@ -1,6 +1,6 @@
 @echo off
 IF %ERRORLEVEL% == 0 (
   wsl sudo /etc/init.d/smartdns restart
-)
+)^
 ELSE echo error:%ERRORLEVEL%
 pause
--- script/stop.bat
+++ script/stop.bat
@@ -1,6 +1,6 @@
 @echo off
 IF %ERRORLEVEL% == 0 (
   wsl sudo /etc/init.d/smartdns stop
-)
+)^
 ELSE echo error:%ERRORLEVEL%
 pause
